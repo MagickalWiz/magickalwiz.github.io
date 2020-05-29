d0485311790585abe3309ee4c3c228e6ba16927a
MagickalWiz <54958629+MagickalWiz@users.noreply.github.com>
2020-05-29 14:45:39 -0700

    Add files via upload

--- output.php
+++ output.php
@@ -6,7 +6,7 @@ $photo = $_POST['photo'];
 $file = file_get_contents("input.html");
 $file = str_replace('%name%', $file);
 $file = str_replace('%photo%', '<img src='.$file.' alt="" />'); //Maybe validate and upload the Photo
-file_put_contents($name.'.html',$file,FILE_APPEND); //Creates a file named $name.html which will output the finished document
+file_put_contents('/home/magickalwiz/http/input/'.$name.'.html',$file,FILE_USE_INCLUDE_PATH | FILE_APPEND); //Creates a file named $name.html which will output the finished document
 }
-header('Location: https://magickalwiz.github.io/'.$name.'.html');
+header('Location: http://192.168.1.93/input/'.$name.'.html');
 ?>
