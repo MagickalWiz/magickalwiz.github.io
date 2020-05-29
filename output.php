d81c9ed457571bfe19417e754d28836917e2fd44
MagickalWiz <54958629+MagickalWiz@users.noreply.github.com>
2020-05-29 11:59:19 -0700

    Add files via upload

---
+++ output.php
@@ -0,0 +1,14 @@
+<?php
+if(isset($_POST['submit'])) {
+$name = $_POST['name'];
+$address = $_POST['address'];
+$photo = $_POST['photo'];
+//Read the file which includes the html page in which the data should be filed and should be returned
+$file = file_get_contents("input.html");
+$file = str_replace('%name%', $file);
+$file = str_replace('%address%', $file);
+$file = str_replace('%photo%', '<img src='.$file.' alt="" />'); //Maybe validate and upload the Photo
+file_put_contents($name.'.html',$file,FILE_APPEND); //Creates a file named $name.html which will output the finished document
+}
+header('Location: http://192.168.1.93/'.$name.'.html');
+?>
